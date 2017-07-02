aa61d1782807dc45fbf7c697b16ffa9e297920bb
Grzegorz <grzegorz.rzadca@gmail.com>
2017-07-02 23:40:07 +0200

    First commit

---
+++ src/main/java/com/small_e/Small_E/SmallEApplication.java
@@ -0,0 +1,12 @@
+package com.small_e.Small_E;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallEApplication.class, args);
+	}
+}
---
+++ src/test/java/com/small_e/Small_E/SmallEApplicationTests.java
@@ -0,0 +1,16 @@
+package com.small_e.Small_E;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmallEApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
